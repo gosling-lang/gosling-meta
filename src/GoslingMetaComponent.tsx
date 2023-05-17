cbcc2fa8c79f48aa85c58584da7fb7d6a5c730e3
sehilyi <sehilyi@gmail.com>
2023-05-17 19:09:03 -0400

    chore: add mimimal example with table

--- src/GoslingMetaComponent.tsx
+++ src/GoslingMetaComponent.tsx
@@ -1,7 +1,8 @@
 import { GoslingComponent, type GoslingSpec } from 'gosling.js';
-import React, { useRef } from 'react';
-import './index.css';
+import React, { useRef, useState, useEffect } from 'react';
+import MetaTable from './MetaTable';
 import 'higlass/dist/hglib.css';
+import './index.css';
 
 type MetaSpec = any; // TODO: define the type of spec for metadata vis.
 
@@ -16,8 +17,33 @@ export default function GoslingMetaComponent(props: GoslingMetaComponentProps): 
     // ...
     const gosRef = useRef(null);
     // ... calculate the position of gosling component and metadata vis
-    const gosPos = { left: 100, top: 100 };
-    const metaPos = { left: 100, top: 0 };
+    const metaVisWidth = 600;
+    const gosPos = { left: 100 + metaVisWidth, top: 100 };
+    const metaPos = { left: 100, top: 100 };
+    // ...
+    const detailID = 'detailedView'
+    const circularRadius = 200;
+    const centerRadius = 0.5;
+    const linearHeight = 120;
+    const linearSize = linearHeight / 6;
+    const [data, setData] = useState([]);
+	useEffect(() => {
+		if (!gosRef.current) return;
+		gosRef.current.api.subscribe('rawData', (type, rawdata) => {
+			const range = gosRef.current.hgApi.api.getLocation(detailID).xDomain
+			if (rawdata.data.length > 0 && rawdata.id === detailID && 'Accnum' in rawdata.data[0]) {
+				const dataInRange = rawdata.data.filter(entry => (entry['Gene start'] > range[0]
+                    && entry['Gene start'] < range[1])
+                    || (entry['Gene end'] > range[0]
+                    && entry['Gene end'] < range[1]))
+				const uniqueInRange = dataInRange.filter((v, i, a) => a.findIndex(v2 => (v2['Gene name'] === v['Gene name'])) === i)
+				setData(uniqueInRange)
+			}
+		});
+		return () => {
+			gosRef.current.api.unsubscribe('rawData');
+		}
+	}, [gosRef]);
     // ...
 
     return (
@@ -31,7 +57,7 @@ export default function GoslingMetaComponent(props: GoslingMetaComponentProps): 
                 />
             </div>
             <div id="metavis-component-wrapper" style={{ ...metaPos }}>
-                <h3>Placeholder of Metadata Vis</h3>
+                <MetaTable data={data} width={metaVisWidth} height={646}/>
             </div>
         </div>
     );
