4754d64fec9e6030c91e15e5822cdd190d543ba5
ThHarbig <theresa_ha@hotmail.de>
2024-06-25 09:54:11 +0200

    feat: Jump linking table, virtualized tanstack table (#4)
    * feat: add parent component for examples
    
    * feat: add PhyloTree
    
    * feat: write a wrapper for gosling component to avoid passing down the gosling ref
    
    * fix: jsdocs
    
    * feat: set states for track height and gosling height
    
    * feat: create meta component wrapper
    
    * feat: adapt table to new API
    
    * feat: add very simple example to demonstrate track issue
    
    * feat: adapt metatable to new events and fixed track ids
    
    * feat: adapt code to new API
    
    * feat: Add sortable table
    
    * feat: fix import
    
    * chore: add TanStack Table Props interface and increase gosling.js version
    
    * feat: add jump button to table
    
    * feat: add linkageType for table (scroll, window, jump)
    
    * feat: implement jump linkage
    
    * fix: adapt to new gosling version
    
    * feat: virtualize metadata table
    
    * fix: Typescript issues
    
    * chore: add default parameter for linkageType
    
    * chore: add eslint rules

--- demo/App.jsx
+++ demo/App.jsx
@@ -2,31 +2,39 @@ import * as React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import IslandViewer from './IslandViewer';
 import 'higlass/dist/hglib.css';
-import GeneSpy from "./GeneSpy";
+import GeneSpy from './GeneSpy';
 
 // The full list of examples
 const examples = {
-	IslandViewer: <IslandViewer/>,
-	GeneSpy: <GeneSpy/>,
-}
+    IslandViewer: <IslandViewer />,
+    GeneSpy: <GeneSpy />
+};
 
 function App() {
-	return (
-		<div className='flex flex-row h-full w-full'>
-			<div className='flex-none border-r-[1px]'>
-				<div className='font-bold font-lg m-3'>Examples</div>
-				<ol className='list-decimal list-inside divide-y divide-solid'>
-					{Object.entries(examples).map(entry => <li className='p-3' key={entry[0]}><Link className='hover:underline' to={`/${entry[0].replace(' ', '_')}`}>{entry[0]}</Link></li>)}
-				</ol>
-			</div>
-			<div className=''>
-				<Routes>
-					<Route path="/" element={examples.IslandViewer} />
-					{Object.entries(examples).map(entry => <Route key={entry[0]} path={`/${entry[0].replace(' ', '_')}`} element={entry[1]}/>)}
-				</Routes>
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex flex-row h-full w-full">
+            <div className="flex-none border-r-[1px]">
+                <div className="font-bold font-lg m-3">Examples</div>
+                <ol className="list-decimal list-inside divide-y divide-solid">
+                    {Object.entries(examples).map(entry => (
+                        <li className="p-3" key={entry[0]}>
+                            <Link className="hover:underline" to={`/${entry[0].replace(' ', '_')}`}>
+                                {entry[0]}
+                            </Link>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+            <div className="">
+                <Routes>
+                    <Route path="/" element={examples.IslandViewer} />
+                    {Object.entries(examples).map(entry => (
+                        <Route key={entry[0]} path={`/${entry[0].replace(' ', '_')}`} element={entry[1]} />
+                    ))}
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/MetaTable.tsx
+++ src/MetaTable.tsx
@@ -1,29 +1,42 @@
-import React, {useCallback, useMemo} from 'react';
-import {mergeData, renameColumns} from "./table-data-transform";
-import type {Datum, DataDeep} from 'gosling.js/dist/src/gosling-schema';
+import React, { useCallback, useMemo } from 'react';
+import { mergeData, renameColumns } from './table-data-transform';
+import type { DataDeep, Datum } from 'gosling.js/dist/src/gosling-schema';
+import TanStackTable from './TanStackTable';
 
 export type MetaTableSpec = {
-    type: "table",
-    // TODO: allow custom data specification for metatable
-    data?: DataDeep;
+    type: 'table';
+    data: DataDeep;
     dataTransform: tableDataTransform[];
     genomicColumns: [string] | [string, string];
-    metadataColumns: { type: 'genomic' | 'nominal' | 'quantitative', columnName: string, columnFormat: string }[];
-}
+    chromosomeField: string;
+    metadataColumns: { type: 'genomic' | 'nominal' | 'quantitative'; columnName: string; columnFormat: string }[];
+    linkageType: 'jump' | 'window'; // jump: Click button in the table to jump to a gene in the visualization, window: The table shows only the selected range in the visualization
+    dataId: 'string';
+};
 
-interface MetaTableProps extends Omit<MetaTableSpec, 'type' | 'data'> {
+interface MetaTableProps extends Omit<MetaTableSpec, 'type' | 'data' | 'dataId'> {
     data: Datum[];
-    range: [{ chromosome: string, position: number }, {
-        chromosome: string,
-        position: number
-    }]
+    range: [
+        { chromosome: string; position: number },
+        {
+            chromosome: string;
+            position: number;
+        }
+    ];
     width: number | string;
     height: number | string;
+    setZoomTo: (
+        range: [
+            { chromosome: string; position: number },
+            {
+                chromosome: string;
+                position: number;
+            }
+        ]
+    ) => void;
 }
 
-export type tableDataTransform =
-    | MergeColumnsTransform
-    | RenameColumnsTransform;
+export type tableDataTransform = MergeColumnsTransform | RenameColumnsTransform;
 
 export interface MergeColumnsTransform {
     type: 'merge';
@@ -33,7 +46,7 @@ export interface MergeColumnsTransform {
 }
 
 export interface RenameColumnsTransform {
-    type: 'rename',
+    type: 'rename';
     fields: string[];
     newFields: string[];
 }
@@ -44,84 +57,103 @@ export interface RenameColumnsTransform {
  * @constructor
  */
 export default function MetaTable(props: MetaTableProps) {
-    const {data, range, dataTransform, genomicColumns, metadataColumns, width, height} = props;
-    const transformData = useCallback((data) => {
-        let dataTransformed: Datum[] = Array.from(data);
-        dataTransform.forEach(transform => {
-            switch (transform.type) {
-                case("merge"):
-                    dataTransformed = mergeData(transform, data);
-                    break;
-                case("rename"):
-                    dataTransformed = renameColumns(transform, data);
-                    break;
-            }
-        })
-        return (dataTransformed);
-    }, [dataTransform]);
+    const {
+        data,
+        range,
+        dataTransform,
+        genomicColumns,
+        chromosomeField,
+        metadataColumns,
+        linkageType = 'window',
+        width,
+        height,
+        setZoomTo
+    } = props;
+    const transformData = useCallback(
+        data => {
+            let dataTransformed: Datum[] = Array.from(data);
+            dataTransform.forEach(transform => {
+                switch (transform.type) {
+                    case 'merge':
+                        dataTransformed = mergeData(transform, data);
+                        break;
+                    case 'rename':
+                        dataTransformed = renameColumns(transform, data);
+                        break;
+                }
+            });
+            return dataTransformed;
+        },
+        [dataTransform]
+    );
     const dataInRange = useMemo(() => {
-        let inRange: Datum[];
-        // features have start and end
-        if (genomicColumns.length === 2) {
-            const start = genomicColumns[0];
-            const end = genomicColumns[1];
-            inRange = data.filter(
-                entry =>
-                    (Number(entry[start]) > range[0].position && Number(entry[start]) < range[1].position) ||
-                    (Number(entry[end]) > range[0].position && Number(entry[end]) < range[1].position)
-            );
-            // features have only start (point features)
-        } else {
-            const position = genomicColumns[0];
-            inRange = data.filter(
-                entry => Number(entry[position]) > range[0].position && Number(entry[position]) < range[1].position
-            );
+        switch (linkageType) {
+            case 'window': {
+                let inRange: Datum[];
+                // features have start and end
+                if (genomicColumns.length === 2) {
+                    const start = genomicColumns[0];
+                    const end = genomicColumns[1];
+                    inRange = data.filter(
+                        entry =>
+                            (Number(entry[start]) > range[0].position && Number(entry[start]) < range[1].position) ||
+                            (Number(entry[end]) > range[0].position && Number(entry[end]) < range[1].position)
+                    );
+                    // features have only start (point features)
+                } else {
+                    const position = genomicColumns[0];
+                    inRange = data.filter(
+                        entry =>
+                            Number(entry[position]) > range[0].position && Number(entry[position]) < range[1].position
+                    );
+                }
+                const uniqueInRange = inRange.filter(
+                    (v, i, a) => a.findIndex(v2 => JSON.stringify(v2) === JSON.stringify(v)) === i
+                );
+                return transformData(uniqueInRange);
+            }
+            case 'jump':
+                return transformData(data);
         }
-        const uniqueInRange = inRange.filter(
-            (v, i, a) => a.findIndex(v2 => JSON.stringify(v2) === JSON.stringify(v)) === i
-        );
-        return (transformData(uniqueInRange));
-    }, [genomicColumns, data, range])
+    }, [genomicColumns, data, range]);
     const columnNames = useMemo(() => {
         return metadataColumns.map(d => d.columnName) ?? (dataInRange.length > 0 ? Object.keys(dataInRange[0]) : []);
-    }, [metadataColumns, dataInRange])
+    }, [metadataColumns, dataInRange]);
+    const jump = useCallback(
+        row => {
+            const pos1 = {
+                chromosome: String(row[chromosomeField]),
+                position: Number(row[genomicColumns[0]])
+            };
+            if (genomicColumns.length == 2) {
+                const pos2 = {
+                    chromosome: String(row[chromosomeField]),
+                    position: Number(row[genomicColumns[1]])
+                };
+                setZoomTo([pos1, pos2]);
+            } else {
+                setZoomTo([pos1, pos1]);
+            }
+        },
+        [chromosomeField, genomicColumns]
+    );
     return (
         <>
-            {dataInRange.length === 0 ? null :
+            {dataInRange.length === 0 ? null : (
                 <div
                     style={{
-                        height,
-                        overflowY: 'scroll',
-                        display: 'inline-block',
-                        width: Number(width) - 10,
+                        width: Number(width) - 10
                     }}
                 >
-                    <table className="table-fixed border-collapse border border-slate-400">
-                        <thead className="capitalize">
-                        <tr className="border border-slate-300  bg-slate-100">
-                            {columnNames.map(d => (
-                                <th className="px-1" key={d}>
-                                    {d}
-                                </th>
-                            ))}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {dataInRange.map(d => (
-                            <tr className="border border-slate-300" key={JSON.stringify(d)}>
-                                {columnNames.map(key => {
-                                    return (
-                                        <td className="px-1" key={key}>
-                                            {d[key]}
-                                        </td>
-                                    );
-                                })}
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                    <TanStackTable
+                        data={dataInRange}
+                        columnNames={columnNames}
+                        isJumpable={true}
+                        jump={jump}
+                        height={height}
+                    />
                 </div>
-            }
+            )}
         </>
     );
 }
